588344449f8f50c4d5c2efc5dc2c68b62dbd67e8
unknown <muniyaakter21@gmail.com>
2024-03-04 12:54:04 +0600

    added search functionality

--- script/script.js
+++ script/script.js
@@ -3,33 +3,23 @@ const cardContainer = document.getElementById('card-container')
 const emptyData = document.getElementById('empty-data')
 
 
+
+
+
 let count = 0;
-const loadData =async (categoryName) =>{
+const loadData =async () =>{
     
-    document.getElementById('loading-spinner').style.display = 'block'
-    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${categoryName}`);
+    
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
     const data = await res.json();
     // document.getElementById('loading-spinner').style.display = 'none'
     console.log(data)
-    setTimeout(() => {
-        document.getElementById('loading-spinner').style.display = 'none'
-    }, 2000);
-
-
-    if(data.posts.length === 0){
-        emptyData.style.display = 'block'
-    }
-    else{
-        emptyData.style.display = 'none'
-    }
-   
-
+    
     // console.log(data.posts)
     postCard.innerHTML= '';
-   
+    
     data.posts.forEach(post => {
-       
-        console.log(post.isActive)
+        // console.log(post.isActive)
         // const div = document.createElement('div');
         const divContent = document.createElement('div') 
         divContent.innerHTML = `
@@ -109,24 +99,88 @@ const postAdd = (data,view) =>{
 
 }
 
-// const searchFetch = async (categoryName) =>{
-//     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${categoryName}`)
-//     const data = await res.json();
-//     console.log(data)
-// }
+const searchFetch = async (categoryName) =>{
+    document.getElementById('loading-spinner').style.display = 'block'
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${categoryName}`)
+    const data = await res.json();
+    console.log(data)
+    setTimeout(() => {
+        document.getElementById('loading-spinner').style.display = 'none'
+    }, 2000);
+
+    
+
+    postCard.innerHTML= '';
+    
+    data.posts.forEach(post => {
+
+        
+        
+
+        console.log(post)
+        // const div = document.createElement('div');
+        const divContent = document.createElement('div') 
+        divContent.innerHTML = `
+        
+            <div id="" class="lg:flex gap-5  bg-[#F3F3F5] lg:w-[780px] rounded-2xl p-10">
+            <div class="">
+                <div class="indicator">
+                    <span id='indicate' class="indicator-item badge  ${post.isActive?'bg-green-500':'bg-red-500'}"></span>
+                    <div class="grid w-32 h-32 bg-base-300 place-items-center">
+                        <img src="${post.image}" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- **** -->
+            <div>
+                <div class="flex gap-5">
+                    <p>#${post.category}</p>
+                    <p>Author: ${post.author.name}</p>
+                </div>
+                <h1 class="text-xl font-bold mt-4">${post.title}</h1>
+                <p class="lg:w-[500px] text-[#0C0D2D99] mt-5">${post.description}</p>
+                <hr class="mt-5">
+                <div class="lg:flex items-center justify-between">
+                    <div class="mt-5 flex lg:gap-10 gap-4">
+                        <div class="flex gap-2">
+                            <img src="images/massage.svg" alt="">
+                            <p>${post.comment_count}</p>
+                        </div>
+                        <div id='views' class="flex gap-2 items-center">
+                            <i class="fa-regular fa-eye"></i>
+                            <p>${post.view_count}</p>
+                        </div>
+                        <div id='views' class="flex gap-2 items-center">
+                            <i class="fa-regular fa-clock"></i>
+                            <p>${post.posted_time} min</p>
+                    </div>
+                
+                </div>
+                <div class="mt-5">
+                    <img onclick= "postAdd('${post.title}','${post.view_count}')" src="images/email 1.svg" alt="">
+                </div>   
+            </div>
+            </div>
+        </div>
+        `;
+
+        postCard.appendChild(divContent)
+
+    });
+}
 
 
-const searchBtn = () =>{
+const searchBtn = (post) =>{
     const inputField = document.getElementById('input-field').value; 
-    loadData(inputField)
-    inputField.value ='';
+    searchFetch(inputField)
+    console.log(inputField)
       
 }
 
-// searchFetch()
 
 
-loadData('comedy')
+searchFetch()
+loadData()
 
 
      
